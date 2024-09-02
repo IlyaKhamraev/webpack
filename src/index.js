1568c52f5736ea24c1ddba5446ec49d6bd11c628
Ilya Khamraev <hamraev9517@gmail.com>
2024-09-02 18:33:08 +0500

    init webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const arr = [10, 2];
+
+const sum = (a,b) => a + b;
+
+const result =  sum(...arr);
+
+console.log(result);
+
+const mult = (a,b) => a * b;
+
+console.log(mult(2,5))
+console.log(mult(2,5))
