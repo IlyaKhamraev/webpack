5c619c75f8e7ec619058743221ed5ab6ba1bb9e9
Ilya Khamraev <hamraev9517@gmail.com>
2024-09-03 16:29:08 +0500

    feat(): added prettier and autoprefixer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import './index.html'
-import './style.css'
+import './index.html';
+import './style/normalize.css';
+import './style/style.css';
 
 const arr = [10, 2];
 
-const sum = (a,b) => a + b;
+const sum = (a, b) => a + b;
 
-const result =  sum(...arr);
+const result = sum(...arr);
 
 console.log(result);
 
-const mult = (a,b) => a * b;
+const mult = (a, b) => a * b;
 
-console.log(mult(2,8))
-console.log(mult(2,5))
+console.log(mult(2, 8));
+console.log(mult(2, 5));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const path = require("node:path");
+const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PostCssPresetEnv = require('postcss-preset-env');
 
-const mode = process.env.NODE_ENV || "development";
-const devMode = mode === "development";
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
 const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
@@ -18,19 +19,35 @@ module.exports = {
         // [contenthash] - нужен для того, что бы каждый раз когда изменяются файлы, у сборки будет изменятся имя,
         // таким образом браузер будет брать файлы не из хеша и отправлять клиенту,
         // а будет новые скачивать, если произошли изменения.
-        filename: 'index.[contenthash].js' //
+        filename: 'index.[contenthash].js', //
     },
     plugins: [
-        new HtmlWebpackPlugin({template: path.resolve(__dirname, 'src', 'index.html')}),
-        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'})
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+        new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
     ],
     module: {
-        rules: [{
-            test: /\.html$/i,
-            loader: "html-loader",
-        },{
-            test: /\.css$/i,
-            use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"],
-        },]
-    }
-}
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [PostCssPresetEnv],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
